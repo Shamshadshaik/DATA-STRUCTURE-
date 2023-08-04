fbf0b2db3ba240f95e20f5064bf0511af9713f63
Shamshadshaik <122433903+Shamshadshaik@users.noreply.github.com>
2023-08-04 18:03:02 +0530

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n,i, num;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d numbers:\n", n);
+    for ( i = 0; i < n; i++) {
+        scanf("%d", &num);
+        if (num % 2 == 0) {
+            printf("%d is even.\n", num);
+        } else {
+            printf("%d is odd.\n", num);
+        }
+    }
+    return 0;
+}
